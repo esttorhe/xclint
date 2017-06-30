382c4da162bb39b6b7594a42b0cca40d5b8e5323
Pedro Piñera <pepibumur@gmail.com>
2017-06-30 17:24:03 +0200

    Add commander dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/carambalabs/xcodeproj.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
     ]
 )

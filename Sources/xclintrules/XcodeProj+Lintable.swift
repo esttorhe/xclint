8b033a135a44b156930a17465e23dc9eb3dad511
Pedro Piñera <pepibumur@gmail.com>
2017-10-23 09:46:04 +0200

    [pbxtarget-lintable] Add targets to the XcodeProj lint function

--- Sources/xclintrules/XcodeProj+Lintable.swift
+++ Sources/xclintrules/XcodeProj+Lintable.swift
@@ -6,6 +6,8 @@ extension XcodeProj: Lintable {
     public func lint() -> [LintError] {
         var errors: [LintError] = []
         errors.append(contentsOf: pbxproj.buildFiles.flatMap({ $0.lint(project: pbxproj) }))
+        errors.append(contentsOf: pbxproj.aggregateTargets.flatMap({ $0.lint(project: pbxproj) }))
+        errors.append(contentsOf: pbxproj.nativeTargets.flatMap({ $0.lint(project: pbxproj) }))
         return errors
     }
     
